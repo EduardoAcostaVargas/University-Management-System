87c29f82658fa78132f726258208c248642f20c0
Ejav97 <ejav97@outlook.com>
2026-08-13 12:02:00 -0500

    Updated Course Class.

--- src/Courses/Course.java
+++ src/Courses/Course.java
@@ -3,12 +3,14 @@ package Courses;
 public class Course {
     String courseCode;
     String courseName;
+    String courseSection;
     String professorTeachingCourse;
     int courseCredits;
 
-    public Course(String courseCode, String courseName, String professorTeachingCourse, int courseCredits){
+    public Course(String courseCode, String courseName, String courseSection, String professorTeachingCourse, int courseCredits){
         this.courseCode = courseCode;
         this.courseName = courseName;
+        this.courseSection = courseSection;
         this.professorTeachingCourse = professorTeachingCourse;
         this.courseCredits = courseCredits;
     }
